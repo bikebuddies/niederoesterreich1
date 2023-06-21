d43f799fd341e88a0c4aa5236e531f9627c6ddcf
LauraMap <laura.saxer@student.uibk.ac.at>
2023-06-21 17:01:12 +0200

    L: Wettervorhersage Icons hinzugefügt

--- main.js
+++ main.js
@@ -68,6 +68,48 @@ map.on('locationerror', function (evt) {
     alert(evt.message);
 });
 
+// Wettervorhersage MET Norway
+async function showForecast(url, latlng) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    //console.log(jsondata, latlng);
+
+    let current = jsondata.properties.timeseries[0].data.instant.details;
+    //console.log(current);
+
+    let timestamp = new Date(jsondata.properties.meta.updated_at).toLocaleString();
+
+    let timeseries = jsondata.properties.timeseries;
+
+    let markup = `
+        <h4>Wetter für ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)} (${timestamp})</h4>
+        <table>
+            <tr><td>Luftdruck (hPa)</td><td>${current.air_pressure_at_sea_level}</td></tr>
+            <tr><td>Lufttemperatur (C)</td><td>${current.air_temperature}</td></tr>
+            <tr><td>Bewölkungsgrad (%)</td><td>${current.cloud_area_fraction}</td></tr>
+            <tr><td>Luftfeuchtigkeit (%)</td><td>${current.relative_humidity}</td></tr>
+            <tr><td>Windrichtung (°)</td><td>${current.wind_from_direction}</td></tr>
+            <tr><td>Windgeschwindigkeit (m/s)</td><td>${current.wind_speed}</td></tr>
+        </table>
+    `;
+
+    // Wettersymbole hinzufügen
+    for (let i =0; i<=24; i+=3) {
+        //console.log(timeseries[i]);
+        let icon = timeseries[i].data.next_1_hours.summary.symbol_code;
+        let img = `icons/${icon}.svg`;
+        markup += `<img src="${img}" style="width:32px;" title="${timeseries[i].time.toLocaleString()}">`
+        //console.log(icon, img);
+    }
+    L.popup().setLatLng(latlng).setContent(markup).openOn(themaLayer.forecast);
+}
+
+// auf Kartenklick reagieren (Event via map.on)
+map.on("click", function(evt) {
+    console.log(evt);
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`;
+    showForecast(url, evt.latlng);
+});
 
 //GPX-Tracks
 
